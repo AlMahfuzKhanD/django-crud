716be45710e4926a59ec1f26723700569a6cdff2
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-12-09 17:02:23 +0600

    django crud done

--- company/employees/urls.py
+++ company/employees/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='employees.index'),
+    path('create/', views.create, name='employees.create'),
+    path('edit/<int:id>/', views.edit, name='employees.edit'),
+    path('delete/<int:id>/', views.delete, name='employees.delete'),
 ]
--- company/employees/views.py
+++ company/employees/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee
+from .forms import EmployeeForm
 # Create your views here.
 def index(request):
     employees = Employee.objects.all()
     return render(request, 'employees/index.html', {'employees': employees})
+
+# CREATE
+def create(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employees.index')
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'employees/form.html', {'form': form})
+
+#Update
+def edit(request, id):
+    employee = get_object_or_404(Employee, id=id)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employees.index')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'employees/form.html', {'form': form})
+# Delete
+def delete(request, id):
+    employee = get_object_or_404(Employee, id=id)
+    employee.delete()
+    return redirect('employees.index')
