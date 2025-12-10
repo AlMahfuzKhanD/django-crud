691a1d5db4dac66ee8e3331aa497fc10a7ec1d7a
AlMahfuzKhanD <almahfuz380@gmail.com>
2025-12-10 12:08:51 +0600

    crud with api done

--- company/employees/urls.py
+++ company/employees/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from . import views
+from . import api_views
+# urlpatterns = [
+#     path('', views.index, name='employees.index'),
+#     path('create/', views.create, name='employees.create'),
+#     path('edit/<int:id>/', views.edit, name='employees.edit'),
+#     path('delete/<int:id>/', views.delete, name='employees.delete'),
+# ]
 
 urlpatterns = [
-    path('', views.index, name='employees.index'),
-    path('create/', views.create, name='employees.create'),
-    path('edit/<int:id>/', views.edit, name='employees.edit'),
-    path('delete/<int:id>/', views.delete, name='employees.delete'),
+    path('employees/', api_views.employee_list_create),
+    path('employees/<int:id>/', api_views.employee_detail),
 ]
